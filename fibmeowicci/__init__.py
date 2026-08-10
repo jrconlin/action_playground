8323107365b062056d7581e3404d904e01ebcf2d
jrconlin <me+src@jrconlin.com>
2026-08-10 14:30:44 -0700

    f add comments /formatting

--- fibmeowicci/__init__.py
+++ fibmeowicci/__init__.py
@@ -1,12 +1,15 @@
 from time import sleep
 
 
-def meow(limit: int | None = None):
+def meow(limit: int | None = None, sleep_factor=0.01):
+    """Make with the meows."""
     fibb, follow = 0, 1
     while True:
         if limit and fibb >= limit:
             break
         meows = " ".join(["meow"] * fibb).capitalize()
-        print(f"{meows}.")
+        if len(meows):
+            print(f"{meows}.\n")
         fibb, follow = follow, fibb + follow
-        sleep(fibb * 0.10)
+        if sleep_factor:
+            sleep(fibb * sleep_factor)
--- fibmeowicci/__main__.py
+++ fibmeowicci/__main__.py
@@ -10,6 +10,7 @@ DEFAULT_LOGGING = "ERROR"
 
 
 def init_logging() -> logging.Logger:
+    """Initialize logging based on PYTHON_LOG"""
     logging.basicConfig(
         level=logging.getLevelNamesMapping().get(
             os.environ.get("PYTHON_LOG", DEFAULT_LOGGING).upper(), DEFAULT_LOGGING
@@ -18,21 +19,24 @@ def init_logging() -> logging.Logger:
     return logging.getLogger("fibmeowicci")
 
 
-def get_config():
+def get_config() -> argparse.Namespace:
+    """Fetch configuration values"""
     parser = argparse.ArgumentParser(
         description="Fib_Meow_icci: It's still not dinner time"
     )
     parser.add_argument("--limit", "-d", help="when to give up")
+    parser.add_argument("--sleep_factor", type=float, help="wait between rounds")
 
     return parser.parse_args()
 
 
-def main(args) -> None:
-    meow(limit=args.limit)
+def main(args: argparse.Namespace = get_config()) -> None:
+    """Meow."""
+    logger.info("😺 Is it dinner?")
+    meow(limit=args.limit, sleep_factor=args.sleep_factor)
 
 
-init_logging()
+logger = init_logging()
 
 if __name__ == "__main__":
-    logging.info("😺 Is it dinner?")
-    main(get_config())
+    main()
